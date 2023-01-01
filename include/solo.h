d964044cc3ac110fee06cb903f6528521a3574dc
kye5283 <hehe921115@gmail.com>
2023-01-01 11:25:06 +0800

    1 commit

--- include/solo.h
+++ include/solo.h
@@ -2,40 +2,62 @@
 #include <stdlib.h>
 #include <time.h>
 
-void multiplayer(int x, int y)
+void solo(int x)
 {
 	srand(time(NULL));
-	int n, count = 0;
+	int n, count = 0, i;
 	int min = 1, max = x;
 	int key = rand() % x + 1;
-	int player[100];
-	for (int i = 0; i < y; i++)
-	{
-		player[i] = i + 1;
-	}
 	char c[2] = "y";
-	do {
-		printf("\n數字範圍為 %d 到 %d.\n", min, max);
-		printf("請玩家 %d 輸入你的猜測: ", player[count % y]);
-		scanf("%d", &n);
-		if (n >= min && n <= max)
+
+	do
+	{
+		while (count % 2 == 0)
 		{
-			if (n > key)
-				max = n - 1;
-			else if (n < key)
-				min = n + 1;
-			else
-			{
-				printf("碰!! 玩家 %d輸了!\n\n是否再來一局 (Y/N)? ", player[count % y]);
-				count = -1;
-				min = 1;
-				max = x;
-				scanf("%s", &c);
+			printf("\n數字範圍為 %d 到 %d.\n", min, max);
+			printf("請玩家輸入你的猜測: ");
+			scanf("%d", &n);
+			if (n >= min && n <= max) {
+				if (n > key)
+					max = n - 1;
+				else if (n < key)
+					min = n + 1;
+				else {
+					printf("碰!! 你輸了!\n\n是否再來一局 (Y/N)? ");
+					count = -1;
+					min = 1;
+					max = x;
+					scanf("%s", &c);
+				}
+				count++;
 			}
-			count++;
+			else
+				printf("輸入數值錯誤.\n");
 		}
-		else
-			printf("輸入數值錯誤.\n");
+
+		while (count % 2 == 1)
+		{
+			printf("\n數字範圍為 %d 到 %d.\n", min, max);
+			i = rand() % (max - min + 1) + min;
+			printf("電腦猜 %d\n", i);
+			if (i >= min && i <= max) {
+				if (i > key)
+					max = i - 1;
+				else if (i < key)
+					min = i + 1;
+				else {
+					printf("碰!! 電腦輸了!\n\n是否再來一局 (Y/N)? ");
+					count = -1;
+					min = 1;
+					max = x;
+					scanf("%s", &c);
+				}
+				count++;
+			}
+			else
+				printf("輸入數值錯誤.\n");
+		}
+
+
 	} while (c[0] == 'y' || c[0] == 'Y');
 }
-
--- sourse/f1.c
+++ sourse/f1.c
@@ -4,9 +4,5 @@
 
 void main(void)
 {
-	int a, b, i;
-	printf("½Ð¿é¤J0~a¡Aa=");
-	scanf("%d", &a);
-	solo(a);
 
 }
